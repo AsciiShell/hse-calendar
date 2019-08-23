f3890e32bd0376b1524d246b43b6b6b5cd217e01
asciishell <asciishell@yandex.ru>
2019-08-23 22:29:08 +0400

    Add group method

--- internal/lesson/lesson.go
+++ internal/lesson/lesson.go
@@ -1,6 +1,7 @@
 package lesson
 
 import (
+	"sort"
 	"time"
 
 	"github.com/asciishell/hse-calendar/internal/client"
@@ -18,4 +19,32 @@ type Lesson struct {
 	Stream     string         `json:"stream"`
 	Owner      *client.Client `json:"-" gorm:"NOT NULL"`
 	CreatedAt  time.Time      `json:"created_at" gorm:"NOT NULL"`
+}
+
+const Day = time.Hour * 24
+
+// Group lessons by date
+//
+// Return ordered by date list of structures,
+// where every element ordered by begin time
+func GroupLessons(lessons []Lesson) []GroupedLessons {
+	result := make(map[time.Time][]Lesson)
+	for i := range lessons {
+		day := lessons[i].Begin.Truncate(Day)
+		result[day] = append(result[day], lessons[i])
+	}
+	out := make([]GroupedLessons, 0, len(result))
+	for k := range result {
+		sort.Slice(result[k], func(i, j int) bool {
+			return result[k][i].Begin.Before(result[k][j].Begin)
+		})
+		out = append(out, GroupedLessons{
+			Date:    k,
+			Lessons: result[k],
+		})
+	}
+	sort.Slice(out, func(i, j int) bool {
+		return out[i].Date.Before(out[j].Date)
+	})
+	return out
 }
