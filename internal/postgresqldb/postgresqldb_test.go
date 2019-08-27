b701fcbaca190c60d5b5da1091c42e43f08f1602
asciishell <asciishell@yandex.ru>
2019-08-27 16:33:48 +0400

    Update tests

--- internal/postgresqldb/postgresqldb_test.go
+++ internal/postgresqldb/postgresqldb_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/asciishell/hse-calendar/internal/lesson"
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/asciishell/hse-calendar/pkg/environment"
 
 	"github.com/asciishell/hse-calendar/internal/client"
-	"github.com/asciishell/hse-calendar/pkg/log"
 )
 
 func TestPostgresGormStorage_CreateClient(t *testing.T) {
@@ -21,9 +22,7 @@ func TestPostgresGormStorage_CreateClient(t *testing.T) {
 		Migrate:    true,
 		MigrateNum: 0,
 	})
-	if err != nil {
-		log.New().Fatalf("can't use database:%s", err)
-	}
+	r.NoError(err, "can't use database")
 	defer db.DB.Close()
 
 	c := client.Client{
@@ -32,25 +31,34 @@ func TestPostgresGormStorage_CreateClient(t *testing.T) {
 		HSERuzID:   0,
 		GoogleCode: "testtesttest",
 	}
+	err = db.CreateClient(&c)
+	r.NoError(err, "can't create client")
 	defer func() {
 		db.DB.Delete(c)
 	}()
-	err = db.CreateClient(&c)
-	r.NoError(err)
 	r.NotEqual(0, c.ID, "client ID not set %+v", c)
-	fmt.Printf("client after insert:%+v\n%+v", err, c)
-	cli, err := db.GetClients()
-	fmt.Printf("client select err:%+v\n", err)
-	for i := range cli {
-		less, _ := db.GetLessonsFor(cli[i], time.Date(2019, 9, 2, 0, 0, 0, 0, time.UTC))
+	fmt.Printf("client after insert:%+v", c)
 
-		fmt.Printf("client no:%d: %+v %+v\n", i, cli[i], err)
-		for j := range less.Lessons {
-			fmt.Printf("\t\tlesson:%+v\n", less.Lessons[j])
-
-		}
+	g := lesson.GroupedLessons{
+		Client:     c,
+		ClientID:   c.ID,
+		Day:        time.Date(2019, 9, 2, 0, 0, 0, 0, time.Local),
+		IsSelected: false,
 	}
-	less, err := db.GetLessonsFor(cli[0], time.Date(2019, 9, 2, 0, 0, 0, 0, time.UTC))
-	err = db.SetLessonsFor(cli[0], less)
-	fmt.Printf("set error:%+v\n", err)
+	g.Lessons = append(g.Lessons, lesson.Lesson{
+		Begin: time.Date(2019, 9, 2, 8, 0, 0, 0, time.Local),
+		End:   time.Date(2019, 9, 2, 9, 0, 0, 0, time.Local),
+		Name:  "Test name",
+	})
+	err = db.SetLessonsFor(c, g)
+	r.NoError(err, "can't lessons group")
+	cli, err := db.GetClients()
+	r.NoError(err, "can't get all clients")
+	for i := range cli {
+		less, err := db.GetLessonsFor(cli[i], time.Date(2019, 9, 2, 0, 0, 0, 0, time.UTC))
+		r.NoError(err, "can't get lessons for client")
+		r.True(len(less.Lessons) > 0, "no lessons")
+		err = db.SetLessonsFor(cli[i], less)
+		r.NoError(err, "can't update lessons")
+	}
 }
